c82282c1ed91dc2a0cd25a8090572e5991f8c738
sergioortega18 <sergio_ortega70232@elpoli.edu.co>
2024-12-16 11:49:33 -0500

    cansado jefe

--- src/App.jsx
+++ src/App.jsx
@@ -15,12 +15,13 @@ const App = () => {
       <Navbar />
       
       <Routes>
-        <Route path="/" element={<Home />}/>
+        <Route path="/home" element={<Home />}/>
         <Route path='/services' element={<Services />}/>      
         <Route path='/about' element={<About />}/>
         <Route path='/contact' element={<Contact />} />
 
       </Routes>
+      
       <Footer />
     </div>
   );
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/ignite-media/",
+  base: '/ignite-media/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
